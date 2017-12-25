b5595c9b5ada768d2d11148bbd6c62a09888fd6f
Kevin Gut <kevin.gut@bitm.sg>
2017-12-25 03:58:00 +0100

    Comment ClusterEntry

--- FAT12/ClusterEntry.cs
+++ FAT12/ClusterEntry.cs
@@ -2,15 +2,35 @@
 {
     public struct ClusterEntry
     {
+        /// <summary>
+        /// Raw Value of the Cluster
+        /// </summary>
+        /// <remarks>
+        /// For clusters holding files and directories,
+        /// this points to the next cluster,
+        /// if it is not the last in the chain.
+        /// </remarks>
         public ushort RawValue;
+        /// <summary>
+        /// <see cref="RawValue"/> interpreted as Cluster Status
+        /// </summary>
         public ClusterStatus Status;
 
+        /// <summary>
+        /// Creates a new Cluster Entry from the given Value
+        /// </summary>
+        /// <param name="Raw">Value</param>
         public ClusterEntry(ushort Raw)
         {
             RawValue = Raw;
             Status = GetFat12Status(Raw);
         }
 
+        /// <summary>
+        /// Gets the Cluster Status from a Value
+        /// </summary>
+        /// <param name="u">Value</param>
+        /// <returns>Cluster Status</returns>
         public static ClusterStatus GetFat12Status(ushort u)
         {
             if (u == 0)
